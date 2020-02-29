135807156745fafeb589974a38d4d7130176f03e
joker1007 <kakyoin.hierophant@gmail.com>
2020-03-01 01:51:04 +0900

    Add unit testing but not work

--- src/test/java/org/embulk/output/kafka/TestKafkaOutputPlugin.java
+++ src/test/java/org/embulk/output/kafka/TestKafkaOutputPlugin.java
@@ -1,5 +1,26 @@
 package org.embulk.output.kafka;
 
+import com.google.common.io.Resources;
+import org.embulk.config.ConfigSource;
+import org.embulk.spi.OutputPlugin;
+import org.embulk.test.TestingEmbulk;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class TestKafkaOutputPlugin
 {
+    @Rule
+    public TestingEmbulk embulk = TestingEmbulk.builder()
+            .registerPlugin(OutputPlugin.class, "kafka", KafkaOutputPlugin.class)
+            .build();
+
+    @Test
+    public void testSimpleJson() throws IOException
+    {
+        ConfigSource configSource = embulk.loadYamlResource("config_simple.yml");
+        embulk.runOutput(configSource, Paths.get(Resources.getResource("in1.csv").getPath()));
+    }
 }
